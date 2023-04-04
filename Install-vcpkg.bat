75446494b4e5a4a9cb14d0d94b3a435b30f9d9a2
Saurav280191 <sauravsikarwar28@hotmail.com>
2023-04-05 02:26:12 +0530

    Removed vcpkg as submodule. InstallVcpkg.bat will take care of it's installation

--- Install-vcpkg.bat
+++ Install-vcpkg.bat
@@ -2,9 +2,9 @@
 
 REM Installing vcpkg
 git clone https://github.com/microsoft/vcpkg.git
-git submodule init
-git submodule update 
-echo Added vckpg as submodule
+::git submodule init
+::git submodule update 
+::echo Added vckpg as submodule
 
 REM Running bootstrap script
 cd vcpkg
