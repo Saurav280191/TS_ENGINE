76827bdfd0cf8f3efe85757a724089d06d8aec84
Saurav280191 <sauravsikarwar28@hotmail.com>
2023-04-09 14:50:41 +0530

    Modified build generators and CMake to support vcpkg removal.

--- GenerateVS22Project.bat
+++ GenerateVS22Project.bat
@@ -1,20 +1,14 @@
 @echo off
 
-REM Installing assimp
-cd vcpkg
-vcpkg install assimp:x64-windows --recurse
-::vcpkg install spdlog:x64-windows --recurse
-::vcpkg install imgui:x64-windows --recurse
-echo Installed assimp 
-echo To generate project,
-PAUSE
-
-cd ..
+echo Building assimp
+cd Dependencies\include\assimp
+call Build_vs2022_x64.bat
+cd ..\..\..\
 
 REM Generate Solution
 cmake -G "Visual Studio 17 2022" -A=x64 -B=./build/x64/Debug -DCMAKE_ARCHITECTURE=x64 -DCMAKE_BUILD_TYPE=Debug
 REM Build Binaries
 cmake --build build/x64/Debug --config Debug
 
-copy "vcpkg\installed\x64-windows\debug\bin\assimp-vc143-mtd.dll" "build\x64\Debug\bin"
+copy "Dependencies\include\assimp\build\x64\bin\Debug\assimp-vc143-mtd.dll" "build\x64\Debug\bin"
 PAUSE
