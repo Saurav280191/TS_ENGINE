cae9a96fbdc859be7837133e28f7f509095bc970
Saurav280191 <sauravsikarwar28@hotmail.com>
2023-04-18 01:12:59 +0530

    Moved Batch BatchButton and ButtonHandler class code to Scene class

--- src/SceneManager/Scene.cpp
+++ src/SceneManager/Scene.cpp
@@ -3,10 +3,13 @@
 
 namespace TS_ENGINE
 {
-	Scene::Scene(std::string name)
+	Scene::Scene(std::string name):
+		m_BatchingEnabled(false)
 	{
 		mSceneNode = CreateRef<Node>();
 		mSceneNode->SetName(name);
+
+		m_BatchButton.RegisterClickHandler(std::bind(&ButtonHandler::OnButtonClicked, &mBatchButtonHandler, std::placeholders::_1, std::placeholders::_2));
 	}
 
 	Scene::~Scene()
@@ -15,10 +18,26 @@ namespace TS_ENGINE
 		mSceneNode.reset();
 	}
 
+	void Scene::OnBatched()
+	{
+		mSceneNode->RemoveAllChildren();
+		Ref<Node> batchedNode = Batcher::GetInstance()->GetBatchedNode();
+		mSceneNode->AddChild(batchedNode);
+		batchedNode->SetParentNode(mSceneNode);
+	}
+
+	void Scene::OnUnBatched()
+	{
+		mSceneNode->RemoveChild(Batcher::GetInstance()->GetBatchedNode());
+		Batcher::GetInstance()->GetBatchedNode()->GetTransform()->Reset();
+	}
+
 	void Scene::Draw(Ref<Shader> shader)
 	{
 		if (mSceneNode)
+		{
 			mSceneNode->Draw(Matrix4(1), shader);
+		}
 		else
 			TS_CORE_ERROR("Scene node not set");
 	}
--- src/SceneManager/Scene.h
+++ src/SceneManager/Scene.h
@@ -1,22 +1,83 @@
 #pragma once
 #include "Node.h"
+#include "Renderer/Batcher.h"
 
 namespace TS_ENGINE
 {
+#pragma region Batching classes
+
+	class BatchButton
+	{
+	public:
+		typedef std::function<void(Ref<TS_ENGINE::Shader>& currentShader, std::vector<Ref<TS_ENGINE::Node>> nodes)> ClickHandler;
+
+		void RegisterClickHandler(ClickHandler handler)
+		{
+			mClickHandler = handler;
+		}
+
+		void Click(Ref<TS_ENGINE::Shader>& currentShader, std::vector<Ref<TS_ENGINE::Node>> nodes)
+		{
+			mClickHandler(currentShader, nodes);
+		}
+
+	private:
+		ClickHandler mClickHandler;
+	};
+
+	class ButtonHandler
+	{
+	public:
+		void OnButtonClicked(const Ref<TS_ENGINE::Shader>& currentShader, std::vector<Ref<TS_ENGINE::Node>> nodes)
+		{
+			currentShader->Bind();
+
+			if (!Batcher::GetInstance()->IsBatched())
+			{
+				if (nodes.size() == 0)
+				{
+					TS_CORE_WARN("No gameObjects to batch!");
+					return;
+				}
+
+				TS_CORE_INFO("***Started batching gameobjects***");
+
+				//Reset stats
+				Application::Get().ResetStats();
+
+				if (nodes.size() > 0)
+				{
+					Batcher::GetInstance()->CreateAtlases();
+					Batcher::GetInstance()->StartBatching(nodes);
+				}
+			}
+		}
+	};
+
+#pragma endregion
+
 	class Scene
 	{
 	public:
 		Scene(std::string name);		
 		~Scene();
+	
+		BatchButton m_BatchButton;
+		bool m_BatchingEnabled;
+		
+		void OnBatched();
+		void OnUnBatched();
 
 		const Ref<Node> GetSceneNode() const
 		{
 			return mSceneNode;
 		}
-
+		
 		void Draw(Ref<Shader> shader);
 	private:		
 		Ref<Node> mSceneNode;
+
+		ButtonHandler mBatchButtonHandler;
 	};
 }
 
