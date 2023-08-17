0b65f6685b5282334147d59b258f41643ea4ea4a
Saurav280191 <sauravsikarwar28@hotmail.com>
2023-08-17 23:58:06 +0530

    Added detailed material propeties

--- src/Renderer/Material.cpp
+++ src/Renderer/Material.cpp
@@ -8,30 +8,141 @@ namespace TS_ENGINE {
 	Material::Material(const std::string& name, Ref<Shader> shader) :
 		mAmbientColor(1.0f),
 		mDiffuseColor(1.0f),
+		mDiffuseMapOffset(0),
+		mDiffuseMapTiling(1),
 		mSpecularColor(1.0f),
-		mShininess(16.0f)
+		mSpecularMapOffset(0),
+		mSpecularMapTiling(1),
+		mShininess(16.0f),
+		mNormalMapOffset(0),
+		mNormalMapTiling(1),
+		mBumpValue(1.0f)
 	{
 		mName = name;
 		mShader = shader;
 	}
 
-	void Material::SetAmbientColor(const Vector3& ambientColor)
+	// Ambient
+	void Material::SetAmbientColor(const Vector4& ambientColor)
 	{
 		mAmbientColor = ambientColor;
 	}
-	void Material::SetDiffuseColor(const Vector3& diffuseColor)
+	Vector4 Material::GetAmbientColor() const
+	{
+		return mAmbientColor;
+	}
+	
+	// Diffuse
+	void Material::SetDiffuseColor(const Vector4& diffuseColor)
 	{
 		mDiffuseColor = diffuseColor;
 	}
-	void Material::SetSpecularColor(const Vector3& specularColor)
+	void Material::SetDiffuseMap(const Ref<Texture2D> diffuseMap)
+	{
+		mDiffuseMap = diffuseMap;
+	}
+	void Material::SetDiffuseMapOffset(Vector2 offset)
+	{
+		mDiffuseMapOffset = offset;
+	}
+	void Material::SetDiffuseMapTiling(Vector2 tiling)
+	{
+		mDiffuseMapTiling = tiling;
+	}
+	Ref<Texture2D> Material::GetDiffuseMap() const
+	{
+		return mDiffuseMap;
+	}
+	Vector2 Material::GetDiffuseMapOffset() const
+	{
+		return mDiffuseMapOffset;
+	}
+	Vector2 Material::GetDiffuseMapTiling() const
+	{
+		return mDiffuseMapTiling;
+	}
+	Vector4 Material::GetDiffuseColor() const
+	{
+		return mDiffuseColor;
+	}
+	
+	// Specular
+	void Material::SetSpecularColor(const Vector4& specularColor)
 	{
 		mSpecularColor = specularColor;
+	}
+	void Material::SetSpecularMap(const Ref<Texture2D> specularMap)
+	{
+		mSpecularMap = specularMap;
+	}
+	void Material::SetSpecularMapOffset(Vector2 offset)
+	{
+		mSpecularMapOffset = offset;
+	}
+	void Material::SetSpecularMapTiling(Vector2 tiling)
+	{
+		mSpecularMapTiling = tiling;
 	}
 	void Material::SetShininess(float shininess)
 	{
 		mShininess = shininess;
 	}
+	Vector4 Material::GetSpecularColor() const
+	{
+		return mSpecularColor;
+	}
+	Ref<Texture2D> Material::GetSpecularMap() const
+	{
+		return mSpecularMap;
+	}
+	Vector2 Material::GetSpecularMapOffset() const
+	{
+		return mSpecularMapOffset;
+	}
+	Vector2 Material::GetSpecularMapTiling() const
+	{
+		return mSpecularMapTiling;
+	}
+	float Material::GetShininess() const
+	{
+		return mShininess;
+	}	
 	
+	// Normal
+	void Material::SetNormalMap(const Ref<Texture2D> normalMap)
+	{
+		mNormalMap = normalMap;
+	}
+	void Material::SetNormalMapOffset(Vector2 offset)
+	{
+		mNormalMapOffset = offset;
+	}
+	void Material::SetNormalMapTiling(Vector2 tiling)
+	{
+		mNormalMapTiling = tiling;
+	}
+	void Material::SetBumpValue(const float bumpValue)
+	{
+		mBumpValue = bumpValue;
+	}
+	Ref<Texture2D> Material::GetNormalMap() const
+	{
+		return mNormalMap;
+	}
+	Vector2 Material::GetNormalMapOffset() const
+	{
+		return mNormalMapOffset;
+	}
+	Vector2 Material::GetNormalMapTiling() const
+	{
+		return mNormalMapTiling;
+	}
+	float Material::GetBumpValue() const
+	{
+		return mBumpValue;
+	}
+	
+	//Shader
 	const Ref<Shader>& Material::GetShader()
 	{
 		return mShader;		
--- src/Renderer/Material.h
+++ src/Renderer/Material.h
@@ -2,6 +2,7 @@
 #include "Core/tspch.h"
 #include "Core/Base.h"
 #include <Renderer/Shader.h>
+#include <Renderer/Texture.h>
 
 namespace TS_ENGINE {
 
@@ -11,18 +12,63 @@ namespace TS_ENGINE {
 		Material(const std::string& name, Ref<Shader> shader);
 		virtual ~Material() = default;
 
-		void SetAmbientColor(const Vector3& ambientColor);
-		void SetDiffuseColor(const Vector3& diffuseColor);
-		void SetSpecularColor(const Vector3& specularColor);
-		void SetShininess(float shininess);
+		// Ambient
+		void SetAmbientColor(const Vector4& ambientColor);		
+		Vector4 GetAmbientColor() const;
+		// Diffuse
+		void SetDiffuseColor(const Vector4& diffuseColor);
+		void SetDiffuseMap(const Ref<Texture2D> diffuseMap);
+		void SetDiffuseMapOffset(Vector2 offset);
+		void SetDiffuseMapTiling(Vector2 tiling);
+		Vector4 GetDiffuseColor() const;
+		Ref<Texture2D> GetDiffuseMap() const;
+		Vector2 GetDiffuseMapOffset() const;
+		Vector2 GetDiffuseMapTiling() const;
+		// Specular
+		void SetSpecularColor(const Vector4& specularColor);
+		void SetSpecularMap(const Ref<Texture2D> specularMap);
+		void SetSpecularMapOffset(Vector2 offset);
+		void SetSpecularMapTiling(Vector2 tiling);
+		void SetShininess(float shininess);		
+		Vector4 GetSpecularColor() const;
+		Ref<Texture2D> GetSpecularMap() const;
+		Vector2 GetSpecularMapOffset() const;
+		Vector2 GetSpecularMapTiling() const;
+		float GetShininess() const;
+		// Bump
+		void SetNormalMap(const Ref<Texture2D> normalMap);
+		void SetNormalMapOffset(Vector2 offset);
+		void SetNormalMapTiling(Vector2 tiling);
+		void SetBumpValue(const float bumpValue);
+		Ref<Texture2D> GetNormalMap() const;
+		Vector2 GetNormalMapOffset() const;
+		Vector2 GetNormalMapTiling() const;
+		float GetBumpValue() const;		
+		//Shader
 		const Ref<Shader>& GetShader();
+		
 	private:
 		std::string mName;
 		Ref<Shader> mShader;
-		Vector3 mAmbientColor;
-		Vector3 mDiffuseColor;
-		Vector3 mSpecularColor;
-		float mShininess;		
+		
+		// Ambient
+		Vector4 mAmbientColor;
+		// Diffuse
+		Vector4 mDiffuseColor;
+		Ref<Texture2D> mDiffuseMap;
+		Vector2 mDiffuseMapOffset;
+		Vector2 mDiffuseMapTiling;
+		// Specular
+		Vector4 mSpecularColor;
+		Ref<Texture2D> mSpecularMap;
+		Vector2 mSpecularMapOffset;
+		Vector2 mSpecularMapTiling;
+		float mShininess;
+		// Normal
+		Ref<Texture2D> mNormalMap;
+		Vector2 mNormalMapOffset;
+		Vector2 mNormalMapTiling;
+		float mBumpValue;
 	};
 }
 
