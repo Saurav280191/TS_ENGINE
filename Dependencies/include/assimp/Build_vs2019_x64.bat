f4b7c74d629c553bb819a9954bb67993c9e7515c
Saurav280191 <sauravsikarwar28@hotmail.com>
2024-01-21 18:42:07 +0530

    Modified Assimp project generator batch files

--- Dependencies/include/assimp/Build_vs2019_x64.bat
+++ Dependencies/include/assimp/Build_vs2019_x64.bat
@@ -2,7 +2,7 @@ SET SOURCE_DIR=.
 SET GENERATOR=Visual Studio 16 2019
 
 SET BINARIES_DIR="./build/x64"
-cmake . -G "%GENERATOR%" -A x64 -S %SOURCE_DIR% -B %BINARIES_DIR%
+cmake . -G "%GENERATOR%" -DASSIMP_NO_EXPORT=ON -A x64 -S %SOURCE_DIR% -B %BINARIES_DIR%
 cmake --build %BINARIES_DIR% --config debug
 cmake --build %BINARIES_DIR% --config release
 
