ebbeda661d45934c3e0d24565f20b133d924b301
Saurav Sikarwar <Saurav.Sikarwar@mds.ac.nz>
2024-12-09 20:43:03 +1300

    Added code to dump memory leaks in EntryPoint header

--- src/Core/EntryPoint.h
+++ src/Core/EntryPoint.h
@@ -1,5 +1,10 @@
 #pragma once
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#endif
+
 #include "Core/Application.h"
+#include <crtdbg.h>
 
 #ifdef TS_PLATFORM_WINDOWS
 
@@ -7,10 +12,14 @@ extern TS_ENGINE::Application* TS_ENGINE::CreateApplication(std::filesystem::pat
 
 int main(int argc, char** argv)
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	TS_ENGINE::Log::Init();
 	auto app = TS_ENGINE::CreateApplication(std::filesystem::path(argv[0]).parent_path());
 	app->Run();
 	delete app;
+	
+	_CrtDumpMemoryLeaks();
 }
 
 #endif
