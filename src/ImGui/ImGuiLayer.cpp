a4f2fb071e730cc9e831b08cf5a534e6e29479d8
Saurav Sikarwar <SauravSikarwar28@hotmail.com>
2025-01-07 13:51:36 +1300

    Commented io.ConfigFlags to avoid issues related to keys not working in Editor Mode.

--- src/ImGui/ImGuiLayer.cpp
+++ src/ImGui/ImGuiLayer.cpp
@@ -22,7 +22,7 @@ namespace TS_ENGINE
 	{
 		ImGui::CreateContext();
 		ImGuiIO& io = ImGui::GetIO(); (void)io; 
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 		//io.ConfigFlags != ImGuiConfigFlags_NavEnableKeyboard;
 		//io.ConfigFlags != ImGuiConfigFlags_DockingEnable;
 		//io.ConfigFlags != ImGuiConfigFlags_ViewportsEnable;
