9a1dc6b33d5c4a3bf52a21e342a32703129ff95a
Saurav Sikarwar <SauravSikarwar28@hotmail.com>
2025-02-12 19:17:32 +1300

    Updated Skybox class to make it behave like a Node parented under scene

--- src/Primitive/Skybox.cpp
+++ src/Primitive/Skybox.cpp
@@ -11,16 +11,19 @@ namespace TS_ENGINE {
 		mSkyTexture = TS_ENGINE::Texture2D::Create(Application::s_AssetsDir.string() + "\\Textures\\Skybox\\industrial_sunset_puresky.jpg");
 		
 		// Create skybox mesh
-		mMesh = CreateRef<Sphere>()->GetMesh();
-		mMesh->SetName("Skybox");
+		Ref<Mesh> sphereMesh = CreateRef<Sphere>()->GetMesh();
+		sphereMesh->SetName("Skybox");
 		
 		// Set material for mesh
-		Ref<Material> unlitMaterial = TS_ENGINE::MaterialManager::GetInstance()->GetUnlitMaterial();
-		mMesh->SetMaterial(unlitMaterial);
+		Ref<Material> unlitMaterial = CreateRef<Material>(*TS_ENGINE::MaterialManager::GetInstance()->GetMaterial("Unlit"));
+		sphereMesh->SetMaterial(unlitMaterial);
 
 		// Set material texture
-		mMesh->GetMaterial()->SetAmbientColor(Vector4(0.8f, 0.8f, 0.8f, 1));
-		mMesh->GetMaterial()->SetDiffuseMap(mSkyTexture);
+		sphereMesh->GetMaterial()->SetAmbientColor(Vector4(0.8f, 0.8f, 0.8f, 1));
+		sphereMesh->GetMaterial()->SetDiffuseMap(mSkyTexture);
+
+		// Add sphere mesh to node
+		AddMesh(sphereMesh);
 
 		// Set Transform
 		mTransform = CreateRef<Transform>();
@@ -28,11 +31,6 @@ namespace TS_ENGINE {
 		mTransform->SetLocalEulerAngles(Vector3(90.0f, 235.0f, 0.0f));
 
 		mTransform->ComputeTransformationMatrix(nullptr);
-
-#ifdef TS_ENGINE_EDITOR
-		// Register entity
-		mEntity = EntityManager::GetInstance()->Register("Skybox", EntityType::SKYBOX);
-#endif
 	}
 
 	Skybox::~Skybox()
@@ -46,20 +44,21 @@ namespace TS_ENGINE {
 		RenderCommand::EnableWireframe(false);
 
 		// Set shader properties for skybox
-		Ref<Shader> shader = mMesh->GetMaterial()->GetShader();
+		Ref<Shader> shader = GetMesh()->GetMaterial()->GetShader();
 
-		// Send Skybox's entityId to vertex shader 
+		// Send Skybox's nodeId to vertex shader 
 #ifdef  TS_ENGINE_EDITOR
-		shader->SetInt("u_EntityID", mEntity->GetEntityID());					// Entity ID
+		shader->SetInt("u_NodeId", mId);										
 #endif
 		// Send Skybox's modelMatrix to vertex shader 
 		shader->SetMat4("u_Model", mTransform->GetWorldTransformationMatrix());	// Model Matrix
 
 		// Render Skybox's mesh 
 #ifdef  TS_ENGINE_EDITOR
-		mMesh->Render(mEntity->GetEntityID(), true);
+		
+		GetMesh()->Render(mId, true);
 #else
-		mMesh->Render(true);
+		GetMesh()->Render(true);
 #endif
 
 		// If wireframe mode is enabled, re-enable it for other meshes
@@ -68,11 +67,4 @@ namespace TS_ENGINE {
 			RenderCommand::EnableWireframe(true);
 		}
 	}
-
-#ifdef TS_ENGINE_EDITOR
-	Ref<Entity> Skybox::GetEntity()
-	{
-		return mEntity;
-	}
-#endif
 }
--- src/Primitive/Skybox.h
+++ src/Primitive/Skybox.h
@@ -1,27 +1,17 @@
 #include "Renderer/Shader.h"
-#include "Primitive/Mesh.h"
 #include "Application.h"
-#include "Transform.h"
+#include "SceneManager/Node.h"
 
 namespace TS_ENGINE
 {
-	class Skybox
+	class Skybox : public Node
 	{
 	public:
 		Skybox();
 		~Skybox();
 
 		void Render();
-
-#ifdef TS_ENGINE_EDITOR
-		Ref<Entity> GetEntity();
-#endif
 	private:
-#ifdef TS_ENGINE_EDITOR
-		Ref<Entity> mEntity;
-#endif
 		Ref<Texture2D> mSkyTexture;
-		Ref<Mesh> mMesh;
-		Ref<Transform> mTransform;
 	};
 }
